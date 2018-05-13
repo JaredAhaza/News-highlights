1ad3a62ad7188d9c5e37a634f1a2a8b0e08e65af
JaredAhaza <jaredahazq@gmail.com>
2018-05-13 12:04:07 +0300

    updated page

---
+++ app/models.py
@@ -0,0 +1,25 @@
+class Source:
+    '''
+    class defining source object
+    '''
+    def __init__(self, id, name, description, url, category, country):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
+
+
+class Articles:
+    '''
+    Defines what our articles will look like
+    '''
+    def __init__(self, id, name, author, title, description, url, publishedAt):
+        self.id = id
+        self.name = name
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.publishedAt = publishedAt
